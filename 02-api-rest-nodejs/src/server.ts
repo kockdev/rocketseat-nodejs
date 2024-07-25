51242da0caa809f8e3b255dbdd9f8f08684ab047
Kock03 <vinikk03@gmail.com>
2024-07-25 13:55:31 -0300

    eslint

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -1,13 +1,15 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
 const app = fastify()
 
 app.get('/hello', () => {
-    return 'Hello world!'
+  return 'Hello world!'
 })
 
-app.listen({
-    port: 3333
-}).then((port) => {
-    console.log('HTTP server running on port: ' + port)
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server Running!')
+  })
